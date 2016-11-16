608b747ecfeeaf985ce2d0526bd92d99542187a9
Jeffrey Hechler <jeffrey.hechler@gmail.com>
2016-11-16 14:39:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  elsif num == 2
+    return true
+  else
+    i = 2
+    while i < num
+      if num%i == 0
+        return false
+      end
+      i += 1
+    end
+  end
+  return true
+end
